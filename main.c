ba4fbc38ec43467d28a343928836ad67735cff91
Johan-Mi <johanmilanov@outlook.com>
2019-12-25 18:47:51 +0100

    -

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
-#include <math.h>
 #include <stdarg.h>
+#include <math.h>
+#include <time.h>
 
 #define MKARR(t,c) (malloc((c)*sizeof(t)))
 
@@ -21,19 +22,22 @@ typedef struct {
 	Layer* layers;
 } NeuralNet;
 
-float sigmoid(float);
-float sigmoidDerivative(float);
+float sigmoid(const float);
+float sigmoidDerivative(const float);
 float randFloat(void);
-Neuron makeNeuron(unsigned);
-Layer makeLayer(unsigned, unsigned);
-NeuralNet makeNeuralNet(unsigned, ...);
+Neuron makeNeuron(const unsigned);
+Layer makeLayer(const unsigned, const unsigned);
+NeuralNet makeNeuralNet(const unsigned, ...);
 void deleteNeuron(Neuron*);
 void deleteLayer(Layer*);
 void deleteNeuralNet(NeuralNet*);
+void think(NeuralNet*, const float*);
 
 
 
 int main(void) {
+	srand(time(NULL));
+
 	NeuralNet net = makeNeuralNet(4, 2, 3, 3, 1);
 
 	deleteNeuralNet(&net);
@@ -42,7 +46,7 @@ int main(void) {
 
 
 
-Neuron makeNeuron(unsigned connectionCount) {
+Neuron makeNeuron(const unsigned connectionCount) {
 	if(connectionCount) {
 		Neuron ret = {0, randFloat(), connectionCount, MKARR(float, connectionCount)};
 		for(unsigned i = 0; i < connectionCount; i++)
@@ -53,14 +57,14 @@ Neuron makeNeuron(unsigned connectionCount) {
 	}
 }
 
-Layer makeLayer(unsigned size, unsigned prevSize) {
+Layer makeLayer(const unsigned size, const unsigned prevSize) {
 	Layer ret = {size, MKARR(Neuron, size)};
 	for(unsigned i = 0; i < size; i++)
 		ret.neurons[i] = makeNeuron(prevSize);
 	return ret;
 }
 
-NeuralNet makeNeuralNet(unsigned layerCount, ...) {
+NeuralNet makeNeuralNet(const unsigned layerCount, ...) {
 	NeuralNet ret = {layerCount, MKARR(Layer, layerCount)};
 	va_list args;
 	va_start(args, layerCount);
@@ -91,14 +95,28 @@ void deleteNeuralNet(NeuralNet* net) {
 	}
 }
 
-float sigmoid(float x) {
+float sigmoid(const float x) {
 	return 1 / (1 - exp(x));
 }
 
-float sigmoidDerivative(float x) {
+float sigmoidDerivative(const float x) {
 	return sigmoid(x) * (1 - sigmoid(x));
 }
 
 float randFloat(void) {
 	return 2 * ((float)rand() / RAND_MAX) - 1;
+}
+
+void think(NeuralNet* net, const float* inputs) {
+	for(unsigned i = 0; i < net->layers[0].size; i++)
+		net->layers[0].neurons[i].activation = inputs[i];
+
+	for(unsigned i = 1; i < net->layerCount; i++) {
+		for(unsigned j = 0; j < net->layers[i].size; j++) {
+			float sum = net->layers[i].neurons[j].bias;
+			for(unsigned k = 0; k < net->layers[i].neurons[j].connectionCount; k++)
+				sum += net->layers[i - 1].neurons[k].activation * net->layers[i].neurons[j].weights[k];
+			net->layers[i].neurons[j].activation = sigmoid(sum);
+		}
+	}
 }

50f6c39bef78585804867a0a96d1367d7dfdd8e6
Johan-Mi <johanmilanov@outlook.com>
2019-12-26 20:30:23 +0100

    -

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <math.h>
 #include <time.h>
 
@@ -20,6 +21,10 @@ typedef struct {
 typedef struct {
 	unsigned layerCount;
 	Layer* layers;
+	float* output;
+	unsigned outputCount;
+	float* error;
+	float cost;
 } NeuralNet;
 
 float sigmoid(const float);
@@ -31,7 +36,7 @@ NeuralNet makeNeuralNet(const unsigned, ...);
 void deleteNeuron(Neuron*);
 void deleteLayer(Layer*);
 void deleteNeuralNet(NeuralNet*);
-void think(NeuralNet*, const float*);
+void think(NeuralNet*, const float*, const float*);
 
 
 
@@ -39,6 +44,10 @@ int main(void) {
 	srand(time(NULL));
 
 	NeuralNet net = makeNeuralNet(4, 2, 3, 3, 1);
+
+	float input[] = {1.f, 0.f};
+	float expectedOutput[] = {1.f};
+	think(&net, input, expectedOutput);
 
 	deleteNeuralNet(&net);
 	return EXIT_SUCCESS;
@@ -71,6 +80,9 @@ NeuralNet makeNeuralNet(const unsigned layerCount, ...) {
 	for(unsigned i = 0; i < layerCount; i++)
 		ret.layers[i] = makeLayer(va_arg(args, unsigned), i ? ret.layers[i - 1].size : 0);
 	va_end(args);
+	ret.outputCount = ret.layers[ret.layerCount - 1].size;
+	ret.output = MKARR(float, ret.outputCount);
+	ret.error = MKARR(float, ret.outputCount);
 	return ret;
 }
 
@@ -93,10 +105,12 @@ void deleteNeuralNet(NeuralNet* net) {
 			deleteLayer(&net->layers[i]);
 		free(net->layers);
 	}
+	free(net->output);
+	free(net->error);
 }
 
 float sigmoid(const float x) {
-	return 1 / (1 - exp(x));
+	return 1 / (1 + expf(-x));
 }
 
 float sigmoidDerivative(const float x) {
@@ -107,9 +121,9 @@ float randFloat(void) {
 	return 2 * ((float)rand() / RAND_MAX) - 1;
 }
 
-void think(NeuralNet* net, const float* inputs) {
+void think(NeuralNet* net, const float* input, const float* expectedOutput) {
 	for(unsigned i = 0; i < net->layers[0].size; i++)
-		net->layers[0].neurons[i].activation = inputs[i];
+		net->layers[0].neurons[i].activation = input[i];
 
 	for(unsigned i = 1; i < net->layerCount; i++) {
 		for(unsigned j = 0; j < net->layers[i].size; j++) {
@@ -119,4 +133,32 @@ void think(NeuralNet* net, const float* inputs) {
 			net->layers[i].neurons[j].activation = sigmoid(sum);
 		}
 	}
+
+	net->cost = 0;
+	for(unsigned i = 0; i < net->outputCount; i++) {
+		net->output[i] = net->layers[net->layerCount - 1].neurons[i].activation;
+		net->error[i] = expectedOutput[i] - net->output[i];
+		net->cost += net->error[i] * net->error[i];
+	}
+	net->cost /= net->outputCount;
+
+	putchar('[');
+	for(unsigned i = 0; i < net->layers[0].size; i++) {
+		if(i)
+			putchar(' ');
+		printf("%f", input[i]);
+	}
+	printf("] -> [");
+	for(unsigned i = 0; i < net->outputCount; i++) {
+		if(i)
+			putchar(' ');
+		printf("%f", net->output[i]);
+	}
+	printf("] Expected: [");
+	for(unsigned i = 0; i < net->outputCount; i++) {
+		if(i)
+			putchar(' ');
+		printf("%f", expectedOutput[i]);
+	}
+	printf("] Cost: %f\n", net->cost);
 }
